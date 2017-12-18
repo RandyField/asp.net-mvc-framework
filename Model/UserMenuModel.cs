3d2dbaca8d3e43348a43647ea9a2a840f932ffaa
RandyField <287572291@qq.com>
2017-12-18 09:09:14 +0800

    rebuild solution

---
+++ Model/UserMenuModel.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class UserMenuModel
+    {
+        public string MenuName { get; set; }
+        public string MenuCode { get; set; }
+        public string ParentCode { get; set; }
+        public string LinkAddress { get; set; }
+        public string IconScript { get; set; }
+        public Nullable<int> Sort { get; set; }
+    }
+}
