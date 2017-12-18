bf29a8e36bf0a84ad1a937cf3b5842f3dcfa61e2
RandyField <287572291@qq.com>
2017-12-19 00:39:30 +0800

    jqDatatable ajax

--- UI/Controllers/UserController.cs
+++ UI/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web.Attribute;
 
 namespace UI.Controllers
 {
@@ -13,12 +14,18 @@ namespace UI.Controllers
         /// 用户管理
         /// </summary>
         /// <returns></returns>
+        [AuthorityFilter]
         public ActionResult Index()
         {
             GetMenu(); //获取菜单
             Location("系统管理", "用户管理"); //当前位置
             ViewBag.Title = "User Page - 用户管理"; //页面标题
             return View();
+        }
+
+        public ActionResult Search()
+        {
+            return Json(new { });
         }
 
     }
