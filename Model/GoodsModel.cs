1e89cb4f0ce157e0da0c4894cbbcdb107f8ef428
RandyField <287572291@qq.com>
2018-01-08 17:44:44 +0800

    for vue api

---
+++ Model/GoodsModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class GoodsModel
+    {
+        public string price { get; set; }
+        public string title { get; set; }
+        public string img { get; set; }
+    }
+
+    public class GoodsList
+    {
+       public List<GoodsModel> goods { get; set; }
+    }
+}
